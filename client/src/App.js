e6781396245693e724595d0a53392e2c32ddaca5
Tyler Kennedy <tkennedy118@gmail.com>
2020-08-10 15:25:44 -0500

    add simple navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Sale from './pages/Sale';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Services from './pages/Services';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path={'/sale'} component={Sale} />
-        <Route exact path={'/profile'} component={Profile} />
-        <Route exact path={'/services'} component={Services} />
-        <Route exact path={['/', '/home']} component={Home} />
-        <Route component={Home} />
-      </Switch>
-    </Router>
+    <Nav />
   );
 }
 
--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -7,7 +7,4 @@ export const REMOVE_PRINT = "REMOVE_PRINT";
 export const SET_CURRENT_PRINT = "SET_CURRENT_PRINT";
 export const ADD_ITEM = "ADD_ITEM";
 export const REMOVE_ITEM = "REMOVE_ITEM";
-export const INCREASE = "INCREASE";
-export const DECREASE = "DECREASE";
-export const CHECKOUT = "CHECKOUT";
 export const CLEAR = "CLEAR";
